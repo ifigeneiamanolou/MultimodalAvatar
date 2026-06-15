2f16131b560b6b09624c45267eb62b92dc3db766
Ifigeneia Manolou <ipm33@bath.ac.uk>
2026-06-15 14:11:19 +0300

    Fixed cors issues for the server connection

--- backend/apis/avatar.py
+++ backend/apis/avatar.py
@@ -4,54 +4,83 @@ import numpy as np
 from faster_whisper import WhisperModel
 
 app = FastAPI()
-origins = [
-    "http://localhost:8081"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
+    allow_origins = ["http://localhost:8081"],           # In production, replace with specific origins
     allow_headers = ["*"],
     allow_methods = ["*"],
-    allow_credentials = True
+    allow_credentials = True      
 )
 
-@app.websocket("/wb")
-async def wesocketendpoint(websocket : WebSocket):
-    await websocket.accept()
+class ConnectionManager:        # Class containing the web socket connections
+    def __init__(self):
+        self.active_connections : list[WebSocket] = []
+
+    async def connect(self, connection : WebSocket):
+        await connection.accept()
+        self.active_connections.append(connection)
+
+    async def disconnect(self, connection : WebSocket):
+        self.active_connections.remove(connection)
+
+    async def send_personal(self, connection : WebSocket, data : str):      # Send data to a single client / websocket
+        await connection.send_text(data)
+
+    async def broadcast(self, data : str):
+        for connection in self.active_connections:
+            await connection.send_text(data)
+
+manager = ConnectionManager()
+
+# Load the whisper model
+model = WhisperModel(model_size_or_path="large-v3", compute_type = "float32")        # Connect to strong GPU
+
+# Perform automatic speech recognition using Whisper
+@app.websocket("/asr")
+async def speechRecognition(websocket : WebSocket):
+    await manager.connect(websocket)        # Connect the client to the websocket manager
 
     try:
         while True:
-            # Receive data
-            data = await websocket.receive_bytes()
+            # Receive data from the client
+            data = await websocket.receive_bytes()   
+            await manager.send_personal(websocket, "Received input audio, processing ...")   
 
             # Convert from audio data to numpy integer array
-            audio_data = np.frombuffer(data, dtype = np.int16).astype(np.float32)
+            audio_data = np.frombuffer(data, dtype = np.int16).astype(np.float32);
+            print("data on which to perform suscription", audio_data);
 
             # Perform audio transcription
-            model = WhisperModel(model_size_or_path="large-v3", device="cuda", compute_type="int8_float16") 
-            result = model.transcribe(audio_data)
+            segments, info = model.transcribe(audio_data)
+            text = " ".join([segment.text for segment in segments])
 
             # Send back the result of the transcription to the frontend
-            await websocket.send_data(result["text"])
+            await manager.send_personal(websocket, text)
     except WebSocketDisconnect:
-        print("server disconnected")
+        await manager.disconnect(websocket)
+        print("Client disconnected")
+    except Exception as e:
+        await manager.disconnect(websocket)
+        print(f"Error {e}")
 
-
-@app.get("/speechRecognition")
-async def generateText():
-    pass
-
-@app.post("postSpeech")
+# Download speech data recorded in the backend
+@app.post("/postSpeech")
 async def downloadSpeechText():
     pass
 
-@app.post("postText")
+# Download corresponding text recorded in the backend
+@app.post("/postText")
 async def downloadText():
     pass
 
+# Generate an NLP response given a text or audio user input
 @app.get("/response")
 async def generateTextResponse():
     pass
 
 # Some way to control facial animation on the right of the screen
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host = "127.0.0.1", port = 8000)       # Custom server configuration
--- frontend/my-app/src/app/index.tsx
+++ frontend/my-app/src/app/index.tsx
@@ -1,46 +1,85 @@
-import { View, StyleSheet } from "react-native";
-import { useRef, useState } from 'react';
+import {useEffect, useRef, useState, useCallback} from 'react';
 import '../../global.css';
 
 // Site used for audio recording: https://www.cybrosys.com/blog/how-to-implement-audio-recording-in-a-react-application
+// Connection to web sockets : https://websocket.org/guides/frameworks/react/
+// Web sockets: https://medium.com/@suganthi2496/fastapi-websockets-react-real-time-features-for-your-modern-apps-b8042a10fd90
 
 export default function Index() {
-  const mediaStream = useRef(new MediaStream());        
-  const mediaRecorder = useRef(new MediaRecorder(new MediaStream()));           // Stream of media content (several tracks)
-  const chunks = useRef([new Blob()]);                                          // Recorded audio
-  const [recordedUrl, setRecordedUrl] = useState('');
+  const ws = useRef<WebSocket | null>(null);                                        // Web Socket connection
+  const mediaStream = useRef<MediaStream | null>(null);        
+  const mediaRecorder = useRef<MediaRecorder | null>(null);                         // Stream of media content (several tracks)
+  const chunks = useRef<Blob[]>([]);                                                // Recorded audio
+  const [recordedUrl, setRecordedUrl] = useState<string | null>(null);
+  const [recordedText, setRecordedText] = useState('Waiting for recording'); 
+
+  useEffect(() => {
+    // Use secure WebSocket in production
+    const wsUrl = "ws://127.0.0.1:8000/asr";
+    ws.current = new WebSocket(wsUrl);
+
+    // Handle the event the web socket connection opens
+    ws.current.onopen = () => {
+      console.log("WS open");
+    };
+
+    // Handle the event a message is sent
+    ws.current.onmessage = (event) => {
+      setRecordedText(event.data);    // Record the data received
+    };
+
+    // Handle the event the connection closes
+    ws.current.onclose = () => {
+      console.log("WS closed");
+    };
+
+    ws.current.onerror = (error) => {
+      console.log(error);
+    }
+
+    // Cleanup (no crash if null)
+    return () => {ws.current?.close(1000, "unmounted");};
+  }, []);
 
   const startRecording = async () => {
+    setRecordedText('');
     try {
-       const stream = await navigator.mediaDevices.getUserMedia({audio : true});      // Prompt the user for permission
-       mediaStream.current = stream;                                                  // Interface to easily record media (ie audio)
-       mediaRecorder.current = new MediaRecorder(stream);
+      const stream = await navigator.mediaDevices.getUserMedia({audio : true});      // Prompt the user for permission
+      mediaStream.current = stream;                                                  // Interface to easily record media (ie audio)
+      mediaRecorder.current = new MediaRecorder(stream);
        
-       // Input audio from the user
-       mediaRecorder.current.ondataavailable = (e) => {
-          if(e.data.size > 0){
-            chunks.current.push(e.data);
-          }
-       };
+      // Input audio from the user
+      mediaRecorder.current.ondataavailable = (e) => {
+        if(e.data.size > 0){
+          chunks.current.push(e.data);
+        }
+      };
 
-       // Handle the ending of audio input
-       mediaRecorder.current.onstop = (e) => {
-          const recordedBlob = new Blob(
-            chunks.current, {type : 'audio/webm'}
-          )
-          const url = URL.createObjectURL(recordedBlob);
-          setRecordedUrl(url);
-          chunks.current = [new Blob()];
-       };
+      // Handle the ending of audio input
+      mediaRecorder.current.onstop = async (e) => {
+        const recordedBlob = new Blob(
+          chunks.current, {type : 'audio/webm'}
+        )
+        const data = await recordedBlob.arrayBuffer();
 
-       // Begins recording data into audio blobs
-       mediaRecorder.current.start();
+        if (ws.current?.readyState === WebSocket.OPEN){
+          ws.current.send(data);
+        }
+
+        // Render the audio element in the frontend
+        const url = URL.createObjectURL(recordedBlob);
+        setRecordedUrl(url);
+        chunks.current = [];
+      };
+
+      // Begins recording data into audio blobs
+      mediaRecorder.current.start();
     } catch (error){
-       console.log(error);
+      console.log('Error accessing the microphone', error);
     }
   };
 
-  const stopRecording = () => {
+  const stopRecording = async () => {
     if(mediaRecorder.current && mediaRecorder.current.state == 'recording'){
       mediaRecorder.current.stop();   // Stop media capture
     }
@@ -54,6 +93,7 @@ export default function Index() {
   return (
     <div className = "flex flex-col place-content-end items-start gap-4 p-6 h-full w-fit shadow-md shadow-gray-200 m-4">
       <div className = "flex flex-col gap-4">
+        <p> {recordedText} </p>
         <input type = "text" className = "focus:border-blue-300 border-gray-300 bg-gray-50 text-gray-500 text-sm p-2 border-2 rounded-lg outline-none" defaultValue = "Enter a prompt for the bot "/>
         <audio className = "w-full" controls src = {recordedUrl}/>
         <div className = "flex flex-row items-center p-2 gap-4">
