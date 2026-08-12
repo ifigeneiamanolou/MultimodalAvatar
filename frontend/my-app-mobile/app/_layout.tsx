f2a1dfe9a8aced09ed519e26ee8e4c7af612e825
Ifigeneia Manolou <ipm33@bath.ac.uk>
2026-08-12 13:55:37 +0300

    Added company logo for the application

--- frontend/my-app-mobile/app/_layout.tsx
+++ frontend/my-app-mobile/app/_layout.tsx
@@ -1,11 +1,15 @@
 import '../global.css';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="feedbackModal" options = {{presentation : 'modal'}} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="feedbackModal" options = {{presentation : 'modal'}} />
+      </Stack>
+      <StatusBar style = "dark"/>
+    </>
   );
 }
