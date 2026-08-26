1a184244701367754c95565d9def71fd930c0ddc
Ifigeneia Manolou <ipm33@bath.ac.uk>
2026-08-26 11:10:47 +0300

    Corrected mistakes in the dockerfiles

--- backend/src/server/routes/response.py
+++ backend/src/server/routes/response.py
@@ -10,41 +10,6 @@ router = APIRouter()
 # Templates
 prompt1 = load_template("interview1")
 prompt2 = load_template("interview2")
-    
-@router.post("/response", response_model = ResponseModel)
-async def generateTextResponse(user_input : LLMInput):
-    """ Generates and saves the response to the input text using an LLM
-    Args:
-        user_input (LLMInput): Contains the user text query and the type of the interviewee
-
-    Returns:
-        ResponseModel: Pydantic model with fields success, meta, data and message
-    """
-
-    # Generate response from OpenAI
-    try:
-        response = get_answer_router(
-            input = [m.model_dump() for m in user_input.input],
-            instructions = prompt1 if user_input.interview_type == 1 else prompt2,
-            emotion = user_input.emotion,
-            model = user_input.model
-        )
-    except Exception as e:
-        return{
-            "success" : False,
-            "data" : "",
-            "message" : f"Error during response generation : {e}"
-        }
-    
-    # Save the response to a file                              
-    save(response)
-
-    # Return the response to the frontend server
-    return {
-        "success" : True,
-        "data" : response, 
-        "message" : "Successful answer generation"
-    }
 
 @router.post("/response/stream")
 async def generateTextResponse(user_input :LLMInput):
