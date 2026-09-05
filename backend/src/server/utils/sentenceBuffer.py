755fbe41081a5fc6e64881dac4e0268c9d311893
Ifigeneia Manolou <ipm33@bath.ac.uk>
2026-09-05 16:34:02 +0300

    small changes

--- backend/src/server/utils/sentenceBuffer.py
+++ backend/src/server/utils/sentenceBuffer.py
@@ -1,21 +1,14 @@
+import re
+
 class sentenceBuffer:
     """ Custom buffer to extract full sentences from the NLP response once a punctuation mark or maximum length is
         reached
     """
     def __init__(self):
         self.buffer = ""                                # Buffer for full sentences
-        self.sentence_ends = [".", "!", ":", ";", "?"]
         self.maximum_length = 200                       # Maximum length before a sentence is buffered
         self.minimum_length = 10
 
-    def full_sentence(self) -> bool:
-        """ Checks if the sentence buffer has generated a full sentence or a maximum length is exceeded
-
-        Returns:
-            bool: indicates whether a full sentence has been accumulated
-        """
-        return (any(self.buffer.endswith(end) for end in self.sentence_ends) and len(self.buffer) >= self.minimum_length) or len(self.buffer) >= self.maximum_length
-    
     async def add(self, text : str):
         """ Adds incoming text to the buffer and returns a sentence if a condition is reached 
 
@@ -27,11 +20,33 @@ class sentenceBuffer:
         """
 
         self.buffer += text
+        while True:
+            # Find a match to the given pattern
+            match = re.search(r'(?<=[.!?;:])(?:\s|$)', self.buffer)
 
-        if(self.full_sentence()):
-            sentence = self.buffer
-            self.buffer = "" 
-            yield sentence           
+            # If a match is found send it to the controller
+            if match:
+                end = match.start() + 1
+                sentence = self.buffer[:end].strip()
+
+                # Safety limit for small sentences
+                if(len(sentence) < self.minimum_length):
+                    break
+
+                # Remove from the buffer
+                self.buffer = self.buffer[end:].lstrip()
+                yield sentence        
+                continue
+
+            # Safety limit for long sentences
+            if(len(self.buffer) > self.maximum_length):
+                sentence = self.buffer[:self.maximum_length]      
+                self.buffer = self.buffer[self.maximum_length:]
+
+                if sentence:
+                    yield sentence.strip()   
+                continue
+            break 
 
     async def flush(self):
         """ Return the remaining text from the buffer even if the conditions are not met
@@ -40,8 +55,8 @@ class sentenceBuffer:
             str: the remaining contents in the buffer
         """
         
-        if self.buffer:     # if the buffer is not empty
-            sentence = self.buffer
+        if self.buffer.strip():     # if the buffer is not empty
+            sentence = self.buffer.strip()
             self.buffer = "" 
             yield sentence
 
